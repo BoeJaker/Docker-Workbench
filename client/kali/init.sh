4f0b493835ce6486603659c82eb9db20b3af004b
BoeJaker <j.baker.cwp@gmail.com>
2023-05-25 09:25:51 +0100

    pyenv integration cleanup

--- client/kali/init.sh
+++ client/kali/init.sh
@@ -2,32 +2,46 @@
 
 rm -R /root/.pyenv
 
-# msploitego dependencies 
-sudo apt install zlib1g zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev libreadline-dev
+#pyenv dependencies
+apt-get install python3 python3-pip zlib1g zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev libreadline-dev -y
 curl https://pyenv.run | bash 
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc  
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc  
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc 
+echo ' eval "$(pyenv init -)"' >> ~/.bashrc 
 
-export PATH="$HOME/.pyenv/bin:$PATH"
+export PATH="root/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-# exec $SHELL &
+# eval "$(pyenv virtualenv-init -)"
+
+pyenv install 3.9.0
 pyenv install 2.7.18
 
-pip install deprecated python-libnmap NmapParser psycopg2  
-apt-get install libpq-dev  
+# Switch to python 2.7
+pyenv global 2.7.18
 
-echo "select an install size, light, default or large"
-read mode
+# msploitego dependencies on python 2.7
+pip install deprecated python-libnmap==0.7.0 NmapParser psycopg2  
+apt-get install libpq-dev -y
 
-[ $mode -eq "default" ]{
-    apt-get install kali-linux-default
-}
-[ $mode -eq "large" ]{
-    apt-get install kali-linux-large
-}
+# Switch back to global python
+# pyenv global system
+
+#change python path for
+# cd /msploitego/src/msploitego/transforms 
+# exec /root/change_python_version.sh
+
+
+# echo "select an install size, light, default or large"
+# read mode
+
+# [ $mode -eq "default" ]{
+#     apt-get install kali-linux-default
+# }
+# [ $mode -eq "large" ]{
+#     apt-get install kali-linux-large
+# }
+
 
 # Start VNC session
 (Xvfb :1 -screen 0 1920x1080x16 &) && 
